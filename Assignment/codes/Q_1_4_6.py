3ddb8493d80eaf058a86a3ae2f5c0ea2e91f8713
RajeevKumar2004 <141238628+RajeevKumar2004@users.noreply.github.com>
2023-08-07 16:37:40 +0530

    Add files via upload

--- Assignment/codes/Q_1_4_6.py
+++ Assignment/codes/Q_1_4_6.py
@@ -10,16 +10,16 @@ O = np.array([-53 / 12, 5 / 12])
 dot_pt_O = (B - O) @ ((C - O).T)
 norm_pt_O = np.linalg.norm(B - O) * np.linalg.norm(C - O)
 cos_theta_O = dot_pt_O / norm_pt_O
-angle_BOC = np.degrees(np.arccos(cos_theta_O))
+angle_BOC = round(360-np.degrees(np.arccos(cos_theta_O)),5)  #Round is used to round of number till 5 decimal places
 print("angle BOC = " + str(angle_BOC))
 
 #To find angle BAC
 dot_pt_A = (B - A) @ ((C - A).T)
 norm_pt_A = np.linalg.norm(B - A) * np.linalg.norm(C - A)
 cos_theta_A = dot_pt_A / norm_pt_A
-angle_BAC = np.degrees(np.arccos(cos_theta_A))
-#To check whether the answer is correct or not
+angle_BAC = round(np.degrees(np.arccos(cos_theta_A)),5)  #Round is used to round of number till 5 decimal places
 print("angle BAC = " + str(angle_BAC))
+#To check whether the answer is correct
 if angle_BOC == 2 * angle_BAC:
   print("\nangle BOC = 2 times angle BAC\nHence the give statement is correct")
 else:
